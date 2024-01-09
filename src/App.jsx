8b160f0e2f874a4b268e85c8db5c3261b6c5101a
SBEGENJOV <seydi19981995@gmail.com>
2024-01-09 05:17:33 +0300

    add/ react icon and home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={}></Route>
+        <Route path="/" element={<Homepage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
