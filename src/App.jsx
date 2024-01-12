519656ef249904879c8a2d32a8ab44e297a8f1ad
SBEGENJOV <seydi19981995@gmail.com>
2024-01-12 06:31:14 +0300

    add/public and private navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Login from "./components/Users/Login";
 import UserProfile from "./components/Users/UserProfile";
+import PublicNavbar from "./components/Navbar/PublicNavbar";
+import PrivateNavbar from "./components/Navbar/PrivateNavbar";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const { userAuth } = useSelector((state) => state?.users);
+  const isLogin = userAuth?.userInfo?.token;
   return (
     <BrowserRouter>
+      {isLogin ? <PrivateNavbar /> : <PublicNavbar />}
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/login" element={<Login />}></Route>
